3969357ebcfa110e07a8f5acd16ba2af84fbcc57
saurabhSinghRbz <singhsaurabh162000@gmail.com>
2022-04-14 11:53:16 +0530

    Updating Best Deals page

---
+++ meanBuy/bestDeals.js
@@ -0,0 +1,377 @@
+var productData = [
+  {
+    image: "./bestDealsImage/1.jfif",
+    title: "Women Cotton And Linen Bohemian Beach Skirt",
+    rating: "4.7/5",
+    price: "₹1,299",
+    off: "UP TO 28% OFF",
+  },
+  {
+    image: "./bestDealsImage/2.jfif",
+    title: "Handsfree Earphone",
+    rating: "4.6/5",
+    price: "₹3,199",
+    off: "UP TO 29% OFF",
+  },
+  {
+    image: "./bestDealsImage/3.jfif",
+    title: "Langsdom Neckband Bluetooth",
+    rating: "4.2/5",
+    price: "₹2,199",
+    off: "UP TO 27% OFF",
+  },
+  {
+    image: "./bestDealsImage/4.jfif",
+    title: "400ml Electric Squeezer",
+    rating: "4.6/5",
+    price: "₹2,899",
+    off: "UP TO 28% OFF",
+  },
+  {
+    image: "./bestDealsImage/5.jfif",
+    title: "Mini Amplifier & Subwoofer",
+    rating: "5.0/5",
+    price: "₹1,499",
+    off: "UP TO 40% OFF",
+  },
+  {
+    image: "./bestDealsImage/6.jfif",
+    title: "Mens Luxury Mechanical Watch",
+    rating: "4.9/5",
+    price: "₹8,999",
+    off: "UP TO 40% OFF",
+  },
+  {
+    image: "./bestDealsImage/7.jfif",
+    title: "Women Versatile Straight Suit",
+    rating: "4.7/5",
+    price: "₹6,799",
+    off: "UP TO 35% OFF",
+  },
+  {
+    image: "./bestDealsImage/8.jfif",
+    title: "Jigsaw Puzzle Wooden Board",
+    rating: "4.2/5",
+    price: "₹849",
+    off: "UP TO 29% OFF",
+  },
+  {
+    image: "./bestDealsImage/9.jfif",
+    title: "Women Patchwork Elastic Waist Skirt",
+    rating: "4.7/5",
+    price: "₹1,699",
+    off: "UP TO 23% OFF",
+  },
+  {
+    image: "./bestDealsImage/10.jfif",
+    title: "STARKING Steel Watch",
+    rating: "4.7/5",
+    price: "₹4,699",
+    off: "UP TO 28% OFF",
+  },
+  {
+    image: "./bestDealsImage/11.jfif",
+    title: "Zeblaze Thor 5 Dual Smartwatch",
+    rating: "4.7/5",
+    price: "₹14,599",
+    off: "UP TO 14% OFF",
+  },
+  {
+    image: "./bestDealsImage/12.jfif",
+    title: "400ml Mixer juicer",
+    rating: "4.4/5",
+    price: "₹2,499",
+    off: "UP TO 32% OFF",
+  },
+  {
+    image: "./bestDealsImage/13.jfif",
+    title: "Handheld Electric Juicer",
+    rating: "4.5/5",
+    price: "₹2,399",
+    off: "UP TO 33% OFF",
+  },
+  {
+    image: "./bestDealsImage/14.jfif",
+    title: "Women Quartz Wheel Watch",
+    rating: "5.0/5",
+    price: "₹7,299",
+    off: "UP TO 51% OFF",
+  },
+  {
+    image: "./bestDealsImage/15.jfif",
+    title: "Causal Ink Print Top Suit",
+    rating: "3.8/5",
+    price: "₹6,499",
+    off: "UP TO 35% OFF",
+  },
+  {
+    image: "./bestDealsImage/16.jfif",
+    title: "Women Relogio Feminino Quartz Watch",
+    rating: "4.4/5",
+    price: "₹1,099",
+    off: "UP TO 27% OFF",
+  },
+  {
+    image: "./bestDealsImage/17.jfif",
+    title: "Women High Waist Bandage Maxi Skirt",
+    rating: "4.6/5",
+    price: "₹2,199",
+    off: "UP TO 19% OFF",
+  },
+  {
+    image: "./bestDealsImage/18.jfif",
+    title: "Bluetooth Stereo Earphone",
+    rating: "4.6/5",
+    price: "₹3,199",
+    off: "UP TO 29% OFF",
+  },
+  {
+    image: "./bestDealsImage/19.jfif",
+    title: "Womens Stainless Steel Bracelet Watch",
+    rating: "4.7/5",
+    price: "₹1,549",
+    off: "UP TO 26% OFF",
+  },
+  {
+    image: "./bestDealsImage/20.jfif",
+    title: "Zealot H1 Sport Bluetooth",
+    rating: "4.2/5",
+    price: "₹3,299",
+    off: "UP TO 34% OFF",
+  },
+  {
+    image: "./bestDealsImage/21.jfif",
+    title: "800W Electric Mixer",
+    rating: "4.8/5",
+    price: "₹6,999",
+    off: "UP TO 13% OFF",
+  },
+  {
+    image: "./bestDealsImage/22.jfif",
+    title: "Mini Fruit Squeezer",
+    rating: "4.9/5",
+    price: "₹2,099",
+    off: "UP TO 24% OFF",
+  },
+  {
+    image: "./bestDealsImage/23.jfif",
+    title: "LEMFO LEMD Smart Watch",
+    rating: "4.2/5",
+    price: "₹8,999",
+    off: "UP TO 36% OFF",
+  },
+  {
+    image: "./bestDealsImage/24.jfif",
+    title: "Summer Casual Small Feet Pants",
+    rating: "4.0/5",
+    price: "₹2,999",
+    off: "UP TO 50% OFF",
+  },
+  {
+    image: "./bestDealsImage/25.jfif",
+    title: "PUBG Mobile Gamepad Grip With Joystick Trigger",
+    rating: "4.1/5",
+    price: "₹899",
+    off: "UP TO 31% OFF",
+  },
+  {
+    image: "./bestDealsImage/26.jfif",
+    title: "Fitness Equipment Slimming Belt",
+    rating: "4.6/5",
+    price: "₹799",
+    off: "UP TO 27% OFF",
+  },
+  {
+    image: "./bestDealsImage/27.jfif",
+    title: "Women Chiffon Floral Print Maxi Skirt",
+    rating: "4.7/5",
+    price: "₹1,499",
+    off: "UP TO 25% OFF",
+  },
+  {
+    image: "./bestDealsImage/28.jfif",
+    title: "T9 Wireless Bluetooth",
+    rating: "4.1/5",
+    price: "₹1,899",
+    off: "UP TO 30% OFF",
+  },
+  {
+    image: "./bestDealsImage/29.jfif",
+    title: "Analog Quartz Watch",
+    rating: "4.4/5",
+    price: "₹899",
+    off: "UP TO 31% OFF",
+  },
+  {
+    image: "./bestDealsImage/30.jfif",
+    title: "Wireless Stereo Headset",
+    rating: "4.3/5",
+    price: "₹1,199",
+    off: "UP TO 33% OFF",
+  },
+  {
+    image: "./bestDealsImage/31.jfif",
+    title: "Smart Display Water Bottle",
+    rating: "4.5/5",
+    price: "₹3,499",
+    off: "UP TO 36% OFF",
+  },
+  {
+    image: "./bestDealsImage/32.jfif",
+    title: "Mini Fruit Squeezer",
+    rating: "4.6/5",
+    price: "₹1,499",
+    off: "UP TO 25% OFF",
+  },
+  {
+    image: "./bestDealsImage/33.jfif",
+    title: "Donut Shape Rainbow Backpack",
+    rating: "3.7/5",
+    price: "₹3,499",
+    off: "UP TO 53% OFF",
+  },
+  {
+    image: "./bestDealsImage/34.jfif",
+    title: "Summer Silk and Linen Top Suit",
+    rating: "4.2/5",
+    price: "₹2,099",
+    off: "UP TO 50% OFF",
+  },
+  {
+    image: "./bestDealsImage/35.jfif",
+    title: "Wooden Tree ball Puzzles Game",
+    rating: "3.7/5",
+    price: "₹899",
+    off: "UP TO 31% OFF",
+  },
+  {
+    image: "./bestDealsImage/36.jfif",
+    title: "Women Stainless Steel Creative Quartz Watch",
+    rating: "4.4/5",
+    price: "₹2,499",
+    off: "UP TO 31% OFF",
+  },
+  {
+    image: "./bestDealsImage/37.jfif",
+    title: "Women Loose Cotton Casual Skirt",
+    rating: "4.4/5",
+    price: "₹1,099",
+    off: "UP TO 27% OFF",
+  },
+  {
+    image: "./bestDealsImage/38.jfif",
+    title: "Mini Wireless Earphones",
+    rating: "4.9/5",
+    price: "₹2,799",
+    off: "UP TO 35% OFF",
+  },
+  {
+    image: "./bestDealsImage/39.jfif",
+    title: "Analog Casual Watch",
+    rating: "4.4/5",
+    price: "₹2,499",
+    off: "UP TO 32% OFF",
+  },
+  {
+    image: "./bestDealsImage/40.jfif",
+    title: "Sport Bluetooth Earphone",
+    rating: "4.9/5",
+    price: "₹2,199",
+    off: "UP TO 23% OFF",
+  },
+  {
+    image: "./bestDealsImage/41.jfif",
+    title: "Honor Pro 5.0 Earphone",
+    rating: "4.3/5",
+    price: "₹6,499",
+    off: "UP TO 28% OFF",
+  },
+  {
+    image: "./bestDealsImage/42.jfif",
+    title: "Electric Food Kettle",
+    rating: "4.0/5",
+    price: "₹2,899",
+    off: "UP TO 28% OFF",
+  },
+];
+
+function displayAllProducts(product) {
+  var allProductsDiv = document.getElementById("allProducts");
+  // allProductsDiv.innerHTML = null;
+  product.forEach(function (ele) {
+    var mainDiv = document.createElement("div");
+    mainDiv.id = "productLayout";
+    let div1 = document.createElement("div");
+    div1.id = "imageBox";
+    let div1img = document.createElement("img");
+    div1img.src = ele.image;
+    div1.appendChild(div1img);
+
+    let div2 = document.createElement("div");
+    div2.id = "titleBox";
+    let div2title = document.createElement("p");
+    div2title.innerHTML = ele.title;
+    div2.appendChild(div2title);
+    let div2rating = document.createElement("div");
+    div2rating.id = "ratingBox";
+    let div2point = document.createElement("span");
+    div2point.innerHTML = ele.rating;
+    let div2star = document.createElement("span");
+    div2star.className = "fa fa-star checked";
+    // div2star.innerHTML = &ensp;
+    div2rating.appendChild(div2point);
+    div2rating.appendChild(div2star);
+    div2.appendChild(div2rating);
+    // div2.appendChild(div2star);
+
+    let div3 = document.createElement("div");
+    div3.id = "priceBox";
+    let div3price = document.createElement("p");
+    div3price.innerHTML = ele.price;
+    let div3off = document.createElement("p");
+    div3off.innerHTML = ele.off;
+    div3.appendChild(div3price);
+    div3.appendChild(div3off);
+
+    mainDiv.appendChild(div1);
+    mainDiv.appendChild(div2);
+    mainDiv.appendChild(div3);
+    allProductsDiv.appendChild(mainDiv);
+  });
+}
+displayAllProducts(productData);
+
+var subcategories = [
+  "Keyboards & Mouse",
+  "Home Improvement",
+  "Bluetooth Earphone",
+  "Sporting Bluetooth",
+  "Wireless Chargers",
+  "Wireless Speakers",
+  "School Backpacks",
+  "Fitness Merchandise",
+  "Home Decor",
+  "Kid's Shoes",
+  "Makeup Accessories",
+  "Wired Headphones",
+  "Smart Watches",
+  "Men's Watches",
+  "Electronics",
+  "Wall Art",
+  "Laptops",
+  "Mobiles Accessories",
+  "Power Banks",
+  "Apple Accessories",
+];
+
+function displaySubcategories(sub) {
+  var allCatDiv = document.getElementById("subCat");
+  sub.forEach(function (ele) {
+    let catDiv = document.createElement("p");
+    catDiv.id = "catId";
+    catDiv.innerHTML = ele;
+    allCatDiv.appendChild(catDiv);
+  });
+}
+
+displaySubcategories(subcategories);
